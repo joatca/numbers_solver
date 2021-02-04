991d884f86453be52487cb5645882c8a36d2e20a
Fraser McCrossan <fraser@mccrossan.ca>
2021-02-03 20:41:37 -0500

    forgot copyright notice on solution_sender.dart

--- lib/solution_sender.dart
+++ lib/solution_sender.dart
@@ -1,3 +1,23 @@
+/*
+This file is part of Numbers Solver - solve the Numbers game used in several
+long-running European game shows
+
+Copyright (C) 2021 Fraser McCrossan
+
+flax is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import 'dart:isolate';
 import 'solver.dart';
 
