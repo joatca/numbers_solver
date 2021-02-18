4645af8329fa8202959cb7f7ac7f026f4a09d6ca
Fraser McCrossan <fraser@mccrossan.ca>
2021-02-17 22:29:16 -0500

    refactor the game selection state into a new class GameState

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -23,14 +23,12 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:numbers_solver/game_classes.dart';
-import 'package:more/iterable.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'solution_sender.dart';
 import 'game_classes.dart';
 import 'info_page.dart';
 import 'text_util.dart';
+import 'game_state.dart';
 
-enum EntryMode { normal, scary }
 enum MainMenuOptions { changeMode, about }
 
 class MainPage extends StatefulWidget {
@@ -43,17 +41,6 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> with TextUtil {
-  // first some constants (or effective constants)
-
-  // maximum length of the target number, used for the target textfield
-  static const _maxTargetLength = 3;
-  // maximum length of the source numbers, used for the source textfields in scary mode
-  static const _maxSourceLength = 3;
-  // maximum number of solutions to store
-  static const _maxSolutions = 50; // plenty of options
-  // number of sources required before allowing a solve
-  static const _numSourcesRequired = 6;
-
   // some UI construction constants
   static const _targetWidth = 120.0;
   static const _dividerThickness = 2.0;
@@ -80,13 +67,6 @@ class _MainPageState extends State<MainPage> with TextUtil {
 
   // stuff to generate the number subscript labels
 
-  // characters to use as labels
-  static final int _firstLabelCode = 'A'.codeUnitAt(0);
-  // labels start at zero; the source numbers first and then intermediates, so we want _numSourcesRequired of the first then the
-  // same number of the second
-  static final List<String> _labelLookup =
-      Iterable.generate(_numSourcesRequired * 2, (i) => i + _firstLabelCode).map((code) => String.fromCharCode(code)).toList();
-
   // UI messages
   static const String _instructions = '''
   To solve a Numbers game, select 6 "source" numbers then enter a target number between 100 and 999
@@ -95,21 +75,8 @@ class _MainPageState extends State<MainPage> with TextUtil {
   ''';
   static const String _inProgress = 'Searching...';
 
-  // list to help construct _sourcesAllowed
-  static final List<int> _one2ten = Iterable.generate(10, (i) => i + 1).toList();
-  // how many of each source number are allowed
-  static final List<int> _sourcesAllowed = _one2ten + _one2ten + [25, 50, 75, 100];
-
   // now some genuine state
-
-  // regular or scary mode?
-  EntryMode _entryMode = EntryMode.normal;
-  // the actual target number
-  var _targetNumber = 0;
-  // which sources are currently selected?
-  List<bool> _sourcesSelected = List<bool>.filled(_sourcesAllowed.length, false);
-  // actual numbers - set by the chips as well as manually in scary mode
-  List<int> _sourceNumbers = List<int>.filled(_numSourcesRequired, 0);
+  GameState gameState = GameState();
   // the list of solutions found (so far)
   List<Solution> _solutions = [];
   // easy way to track whether we are running or now, used to tweak the UI
@@ -122,10 +89,6 @@ class _MainPageState extends State<MainPage> with TextUtil {
 
   // to store the theme to avoid calling it all the time
   ThemeData _theme;
-  // used to attempt to dismiss the keyboard once we start solving, and to set the initial target number from saved prefs
-  TextEditingController _targetTextController = TextEditingController();
-  // used to set source numbers from saved prefs
-  List<TextEditingController> _sourceControllers;
   // a filter to prevent entry of anything other than digits, used on all text controllers
   TextInputFormatter _digitsOnlyFormatter;
 
@@ -137,49 +100,13 @@ class _MainPageState extends State<MainPage> with TextUtil {
   @override
   void initState() {
     super.initState();
-    _loadPreviousValues();
-    _sourceControllers = _sourceNumbers.map((s) => TextEditingController()).toList(); // generate enough of them on the fly
+    _initGameState();
     _digitsOnlyFormatter = FilteringTextInputFormatter.digitsOnly;
   }
 
-  void _loadPreviousValues() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      final String sources = prefs.getString('sources');
-      _targetNumber = prefs.getInt('target') ?? 0;
-      if (_targetNumber > 0) {
-        // only try to set the target field if it's > 0, otherwise we can leave it blank
-        _targetTextController.text = _targetNumber.toString();
-      }
-      if (sources != null) {
-        // values are the source numbers comma-separated
-        try {
-          final List<int> nums = sources.split(',').map((s) => int.parse(s)).toList();
-          nums.indexed().forEach((each) {
-            _setSourceNumber(each.index, each.value);
-          });
-          if (!_sourceNumbers.every((sn) => sn == 0 || _sourcesAllowed.any((sa) => sn == sa))) {
-            _entryMode = EntryMode.scary; // we found some numbers outside regular numbers
-          } else {
-            _setIndexesFromNumbers();
-          }
-          maybeSolve();
-        } on Exception catch (e) {
-          // probably some parsing error, we don't care, just fail to load
-          print("exception: $e");
-        }
-      }
-    });
-  }
-
-  void _saveValues() async {
-    if (_entryMode == EntryMode.normal) {
-      _setNumbersFromIndexes();
-    }
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setString('sources', _sourceNumbers.map((n) => n.toString()).join(','));
-    prefs.setInt('target', _targetNumber);
-    prefs.remove('state');
+  void _initGameState() async {
+    await gameState.load(maybeSolve);
+    setState(() {});
   }
 
   @override
@@ -194,13 +121,13 @@ class _MainPageState extends State<MainPage> with TextUtil {
     _dividerColor = _theme.textTheme.button.color.withOpacity(0.2);
     _numberChipUnselectedColor = _theme.backgroundColor;
     _numberChipSelectedColor = _theme.accentColor;
-    final clearable = _sourcesSelected.any((i) => i) || _targetNumber > 0;
+    final clearable = gameState.clearable();
     return Scaffold(
         appBar: AppBar(title: Text(widget.title), actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.clear),
             tooltip: 'Clear',
-            onPressed: clearable ? _resetPuzzle : null,
+            onPressed: clearable ? _reset : null,
           ),
           PopupMenuButton<MainMenuOptions>(
             onSelected: (MainMenuOptions mode) {
@@ -210,21 +137,27 @@ class _MainPageState extends State<MainPage> with TextUtil {
                   MaterialPageRoute(builder: (context) => InfoPage()),
                 );
               } else if (mode == MainMenuOptions.changeMode) {
-                _toggleMode();
+                setState(() {
+                  gameState.toggleMode();
+                });
               }
             },
             itemBuilder: (context) => <PopupMenuEntry<MainMenuOptions>>[
-              PopupMenuItem<MainMenuOptions>(
+              CheckedPopupMenuItem<MainMenuOptions>(
                 enabled: true,
+                checked: gameState.scaryMode,
                 value: MainMenuOptions.changeMode,
                 child: Text(
-                  _entryMode == EntryMode.normal ? 'Scary Mode' : 'Normal Mode',
+                  'Scary Mode',
                 ),
               ),
               PopupMenuItem<MainMenuOptions>(
                 enabled: true,
                 value: MainMenuOptions.about,
-                child: const Text('About'),
+                child: const Text(
+                  'About',
+                  textAlign: TextAlign.end,
+                ),
               ),
             ],
           )
@@ -281,13 +214,13 @@ class _MainPageState extends State<MainPage> with TextUtil {
         padding: EdgeInsets.all(8.0),
         width: _targetWidth,
         child: TextField(
-            maxLength: _maxTargetLength,
+            maxLength: GameState.maxTargetLength,
             style: _targetStyle,
             maxLines: 1,
             showCursor: true,
             textAlign: TextAlign.center,
             keyboardType: TextInputType.number,
-            controller: _targetTextController,
+            controller: gameState.targetTextController,
             inputFormatters: [_digitsOnlyFormatter],
             decoration: InputDecoration(
               hintText: 'Target',
@@ -298,11 +231,10 @@ class _MainPageState extends State<MainPage> with TextUtil {
             onChanged: (String val) async {
               setState(() {
                 if (val.length > 0) {
-                  _targetNumber = int.parse(val);
+                  gameState.targetNumber = int.parse(val);
                 } else {
-                  _targetNumber = 0;
+                  gameState.targetNumber = 0;
                 }
-                _saveValues();
                 _solutions.clear();
                 maybeSolve();
               });
@@ -319,51 +251,40 @@ class _MainPageState extends State<MainPage> with TextUtil {
         : pad(_running ? _inProgress : _instructions, _theme.textTheme.bodyText1);
   }
 
-  // all the list indexes of allowed source numbers
-  Iterable<int> _allIndexes() => Iterable.generate(_sourcesAllowed.length, (i) => i);
-
-  // all the indexes of selected source numbers
-  Iterable<int> _selectedIndexes() => _allIndexes().where((i) => _sourcesSelected[i]);
-
-  // all the indexes of the _sourceNumbers list
-  Iterable<int> _sourceNumberIndexes() => Iterable.generate(_sourceNumbers.length, (i) => i);
-
-  // all the selected numbers
-  Iterable<Value> _selectedValues() => _sourceNumbers.indexed().map((each) => Value(each.value, each.index));
-
-  // how many are selected? not exactly efficient but meh
-  int _countSelected() => _sourceNumbers.where((n) => n != 0).length;
-
   // converts a source number index to a selectable chip
   Widget _numberChip(int index, void Function(bool) action) {
     return Container(
         padding: EdgeInsets.only(left: 1.0, right: 1.0),
         child: FilterChip(
-            selected: _sourcesSelected[index],
+            selected: gameState.sourcesSelected[index],
             showCheckmark: false,
             onSelected: action,
             backgroundColor: _numberChipUnselectedColor,
             selectedColor: _numberChipSelectedColor,
             label: Text(
-              _sourcesAllowed[index].toString(),
+              GameState.sourcesAllowed[index].toString(),
               style: _sourceButtonStyle,
             )));
   }
 
   List<Widget> _allChips() {
-    return _allIndexes().map<Widget>((i) {
+    return gameState.allIndexes().map<Widget>((i) {
       return _numberChip(i, (bool selected) {
-        if (selected) {
-          addSource(i);
-        } else {
-          removeSource(i);
-        }
+        setState(() {
+          if (selected) {
+            gameState.addSource(i);
+            maybeSolve();
+          } else {
+            gameState.removeSource(i);
+            _killSolver();
+          }
+        });
       });
     }).toList();
   }
 
   List<Widget> _allSourceFields() {
-    return _sourceNumberIndexes().map<Widget>((i) {
+    return gameState.sourceNumberIndexes().map<Widget>((i) {
       return _numberField(i);
     }).toList();
   }
@@ -374,13 +295,13 @@ class _MainPageState extends State<MainPage> with TextUtil {
             padding: EdgeInsets.fromLTRB(4, 2, 4, 2),
             width: _targetWidth,
             child: TextField(
-                maxLength: _maxSourceLength,
+                maxLength: GameState.maxSourceLength,
                 style: _sourceStyle,
                 maxLines: 1,
                 showCursor: true,
                 textAlign: TextAlign.center,
                 keyboardType: TextInputType.number,
-                controller: _sourceControllers[index],
+                controller: gameState.sourceControllers[index],
                 inputFormatters: [_digitsOnlyFormatter],
                 decoration: InputDecoration(
                   hintText: '-',
@@ -390,9 +311,7 @@ class _MainPageState extends State<MainPage> with TextUtil {
                 ),
                 onChanged: (String val) async {
                   setState(() {
-                    _sourceNumbers[index] = val.length > 0 ? int.parse(val) : 0;
-                    _setIndexesFromNumbers();
-                    _saveValues();
+                    gameState.setSourceNumber(index, val.length > 0 ? int.parse(val) : 0);
                     _solutions.clear();
                     maybeSolve();
                   });
@@ -400,12 +319,14 @@ class _MainPageState extends State<MainPage> with TextUtil {
   }
 
   Widget _sourceEntryWidgets() {
-    if (_entryMode == EntryMode.normal) {
+    if (gameState.scaryMode) {
+      return Row(
+        children: _allSourceFields(),
+      );
+    } else {
       return Wrap(
         children: _allChips(),
       );
-    } else {
-      return Row(children: _allSourceFields());
     }
   }
 
@@ -421,7 +342,7 @@ class _MainPageState extends State<MainPage> with TextUtil {
           style: _numberStyle,
         ),
         Text(
-          _labelLookup[v.label],
+          gameState.label(v.label),
           style: _tagStyle,
         ),
       ],
@@ -469,7 +390,7 @@ class _MainPageState extends State<MainPage> with TextUtil {
                       color: Colors.green,
                     )
                   : Text(
-                      diffFormat(solution.result - _targetNumber),
+                      diffFormat(solution.result - gameState.targetNumber),
                       style: solution.away < 10 ? _resultUnder10Style : _resultOver10Style,
                     ),
             )),
@@ -490,103 +411,21 @@ class _MainPageState extends State<MainPage> with TextUtil {
     return size.width;
   }
 
-  // change between normal and scary mode
-  void _toggleMode() {
-    setState(() {
-      if (_entryMode == EntryMode.normal) {
-        // switch from normal mode to scary mode
-        _entryMode = EntryMode.scary;
-        _setNumbersFromIndexes();
-      } else {
-        // switch from scary mode to normal mode
-        _entryMode = EntryMode.normal;
-        _setIndexesFromNumbers();
-        _saveValues(); // might have lost some numbers so must update now else the saved values will still include scary numbers
-      }
-    });
-  }
-
-  // do our best to set _sourcesSelected from the current contents of _sourceNumbers
-  void _setIndexesFromNumbers() {
-    _allIndexes().forEach((i) {
-      _sourcesSelected[i] = false;
-    });
-    _sourceNumbers.forEach((element) {
-      if (element != 0) {
-        for (var i in _allIndexes()) {
-          if (_sourcesSelected[i] == false && _sourcesAllowed[i] == element) {
-            _sourcesSelected[i] = true;
-            break;
-          }
-        }
-      }
-    });
-  }
-
-  // do our best to set the numbers from the set index fields (which may mean discarding values that aren't allowed)
-  void _setNumbersFromIndexes() {
-    _sourceNumberIndexes().forEach((i) {
-      _setSourceNumber(i, 0);
-    });
-    var numberIndex = 0;
-    _selectedIndexes().forEach((selectedIndex) {
-      _setSourceNumber(numberIndex++, _sourcesAllowed[selectedIndex]);
-    });
-  }
-
-  // everything needed to set a source value, include the matching text field
-  void _setSourceNumber(int index, int value) {
-    _sourceNumbers[index] = value;
-    _sourceControllers[index].text = value == 0 ? '' : value.toString();
-  }
-
-  // action to take when an unselected chip is pressed
-  void addSource(int index) {
-    if (_countSelected() < _numSourcesRequired) {
-      // only set it if we have less than the limit already
-      setState(() {
-        _sourcesSelected[index] = true;
-        _saveValues(); // copies values to text fields so no need to do it here
-        maybeSolve();
-      });
-    }
-  }
-
-// action to take when a selected chip is pressed
-  void removeSource(int index) {
-    setState(() {
-      _sourcesSelected[index] = false;
-      _saveValues(); // copies values to text fields so no need to do it here
-      _solutions.clear();
-    });
-  }
-
   void maybeSolve() {
     if (_running) {
       _killSolver();
     }
-    final readyToSolve = _countSelected() == _numSourcesRequired && _targetNumber >= 100;
-    final sourcesIncludeTarget = _sourceNumbers.any((n) => n == _targetNumber);
-    if (readyToSolve && !sourcesIncludeTarget) {
+    if (gameState.ready()) {
       _initSolver();
     }
   }
 
   // reset everything - remove any solutions, reset the target to zero and clear all the selected source numbers
-  void _resetPuzzle() {
+  void _reset() {
     setState(() {
       _killSolver();
-      _targetNumber = 0;
-      _selectedIndexes().forEach((i) {
-        _sourcesSelected[i] = false;
-      });
-      _sourceNumberIndexes().forEach((i) {
-        _sourceNumbers[i] = 0;
-        _sourceControllers[i].text = '';
-      });
-      _saveValues();
+      gameState.reset();
       _solutions.clear();
-      _targetTextController.clear();
     });
   }
 
@@ -609,7 +448,7 @@ class _MainPageState extends State<MainPage> with TextUtil {
       _solutions.add(solution);
       // we sort by the closest then by shortest
       _solutions.sort();
-      while (_solutions.length > _maxSolutions) {
+      while (_solutions.length > GameState.maxSolutions) {
         _solutions.removeLast();
       }
     }
@@ -633,7 +472,7 @@ class _MainPageState extends State<MainPage> with TextUtil {
   }
 
   void _hideKeyboard() {
-    if (_entryMode == EntryMode.normal) {
+    if (!gameState.scaryMode) {
       // only tweak the focus if we are in normal mode - it's too annoying in scary mode
       setState(() {
         FocusScope.of(context).unfocus(); // dismiss the keyboard if possible
@@ -647,7 +486,7 @@ class _MainPageState extends State<MainPage> with TextUtil {
     _solutions.clear();
     _sendToSolver = await _startSolverListener();
     // we now have a running isolate and a port to send it the game
-    _sendToSolver.send({'numbers': _selectedValues().toList(), 'target': _targetNumber});
+    _sendToSolver.send({'numbers': gameState.selectedValues().toList(), 'target': gameState.targetNumber});
     setState(() {
       _running = true;
     });
